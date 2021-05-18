dd020eeff2b50b32cba8adfee343ccd6bc0f31c1
Matt Plog <ps3hawk@gmail.com>
2021-05-18 16:01:36 -0500

    First

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -37,6 +37,25 @@ public class CoreCompetency {
         return Objects.hash(id);
     }
 
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public static void setNextId(int nextId) {
+        CoreCompetency.nextId = nextId;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
 }
